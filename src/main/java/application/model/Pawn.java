65a553c0af46da4f4227c56600ce088ceb8f465a
mderouet <martin.derouet0@gmail.com>
2019-02-07 19:47:23 +0100

    Unit testing and Pawn behavior implementation

--- src/main/java/application/model/Pawn.java
+++ src/main/java/application/model/Pawn.java
@@ -1,6 +1,12 @@
 package application.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import application.utils.Utility;
 
 public class Pawn extends Piece {
 
@@ -16,6 +22,74 @@ public class Pawn extends Piece {
 	@Override
 	public boolean move(Map<String, Piece> board, String squareCoordDeparture, String squareCoordArrival) {
 		System.out.println("Je bouge comme un pion");
-		return false;
+
+		// A pawn can only go forward we need the color in order to know whether we are
+		// going forward or not
+
+		Color myColor = board.get(squareCoordDeparture).getColor();
+
+		List<String> xArray = new ArrayList<String>();
+		xArray.addAll(Arrays.asList("--A", "-A", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"));
+
+		String xCoord = Utility.getLetterFromNumber(squareCoordDeparture);
+		int yCoord = Utility.getNumberFromString(squareCoordDeparture);
+
+		// White pawn can only increase his ordonate
+		if (myColor == Color.WHITE && Utility.getNumberFromString(squareCoordArrival) < yCoord) {
+			return false;
+		}
+		// Black pawn can only decrease his ordonate
+		if (myColor == Color.BLACK && Utility.getNumberFromString(squareCoordArrival) > yCoord) {
+			return false;
+		}
+
+		// At this stage our pawn goes to the right direction
+
+		List<String> positionReachableByPawn = new ArrayList<>();
+		if (myColor == Color.WHITE) {
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord) - 1) + (yCoord + 1)));
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord)) + (yCoord + 1)));
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord) + 1) + (yCoord + 1)));
+			if (yCoord == 2) {
+				positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord)) + (yCoord + 2)));
+			}
+		}
+		if (myColor == Color.BLACK) {
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord) + 1) + (yCoord - 1)));
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord)) + (yCoord - 1)));
+			positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord) - 1) + (yCoord - 1)));
+			if (yCoord == 7) {
+				positionReachableByPawn.add((xArray.get(xArray.indexOf(xCoord)) + (yCoord - 2)));
+			}
+		}
+
+		// Find possible moves
+		Iterator<String> it = positionReachableByPawn.iterator();
+		while (it.hasNext()) {
+			// Position must exist on the board
+			String position = it.next();
+
+			// Delete position if it's not even on the board (overflow array behavior)
+			if (!board.containsKey(position)) {
+				it.remove();
+				
+				// The positions exists on the board
+			} else {
+				// Same column of departure / arrival
+				// Arrival position must be Null (pawn behavior)
+				if ((Utility.getLetterFromNumber(position).contains(xCoord)) && (board.get(position) != null)) {
+					it.remove();
+				}
+
+				// Diagonal left / right ?
+				// Checking if we can "eat" something in diagonal
+				if (!(Utility.getLetterFromNumber(position).contains(xCoord))
+						&& ((board.get(position) == null) || (board.get(position).getColor() == myColor))) {
+					it.remove();
+				}
+			}
+		}
+
+		return positionReachableByPawn.contains(squareCoordArrival);
 	}
 }
--- src/test/java/springboot/test/controller/ChessControllerTest.java
+++ src/test/java/springboot/test/controller/ChessControllerTest.java
@@ -13,164 +13,178 @@ public class ChessControllerTest {
 	// First turn white can play
 	@Test(expected = NotYourTurnException.class)
 	public void whiteTurnToPlay() throws Exception {
-		Board board = new Board("David", "Goliath");
-		board.move("Goliath", "C1", "C2");
+		Board board = new Board("Promethee", "Epimethee");
+		board.move("Epimethee", "C1", "C2");
 	}
 
 	// Player name does not exist
 	@Test(expected = NotYourTurnException.class)
 	public void playerName() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.move("noname", "C1", "C2");
 	}
 
 	// Departure square must contain a piece
 	@Test(expected = MoveImpossibleException.class)
 	public void departurePieceExist() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		// After init, C4 does not contain any piece
-		board.move("David", "C4", "C2");
+		board.move("Promethee", "C4", "C2");
 	}
 	
 	// Departure square must contain a piece
 	@Test(expected = MoveImpossibleException.class)
 	public void squaresExistOnTheBoard() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		// The board does not contain C9 position nor I6
-		board.move("David", "C9", "I6");
+		board.move("Promethee", "C9", "I6");
 	}
 	
 	@Test(expected = MoveImpossibleException.class)
 	public void sameSquare() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		// The board does not contain C9 position nor I6
-		board.move("David", "A2", "A2");
+		board.move("Promethee", "A2", "A2");
 	}
 	
 	@Test
 	public void rookMove() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.setBoard(BoardBuilder.initializeTestPosition());
 		board.displayBoard();
 		
 		// Wrong moves
-		assertEquals(false, board.move("David", "D4", "A4"));
-		assertEquals(false, board.move("David", "D4", "G4"));
-		assertEquals(false, board.move("David", "D4", "G5"));
-		assertEquals(false, board.move("David", "D4", "A3"));
-		assertEquals(false, board.move("David", "D4", "A5"));
-		assertEquals(false, board.move("David", "D4", "D8"));
-		assertEquals(false, board.move("David", "D4", "D7"));
-		assertEquals(false, board.move("David", "D4", "D3"));
+		assertEquals(false, board.move("Promethee", "D4", "A4"));
+		assertEquals(false, board.move("Promethee", "D4", "G4"));
+		assertEquals(false, board.move("Promethee", "D4", "G5"));
+		assertEquals(false, board.move("Promethee", "D4", "A3"));
+		assertEquals(false, board.move("Promethee", "D4", "A5"));
+		assertEquals(false, board.move("Promethee", "D4", "D8"));
+		assertEquals(false, board.move("Promethee", "D4", "D7"));
+		assertEquals(false, board.move("Promethee", "D4", "D3"));
 
 		// Right moves
-		assertEquals(true, board.move("David", "D4", "C4"));
-		assertEquals(true, board.move("David", "D4", "D6"));
-		assertEquals(true, board.move("David", "D4", "F4"));
-		assertEquals(true, board.move("David", "D4", "E4"));
-		assertEquals(true, board.move("David", "D4", "D5"));
+		assertEquals(true, board.move("Promethee", "D4", "C4"));
+		assertEquals(true, board.move("Promethee", "D4", "D6"));
+		assertEquals(true, board.move("Promethee", "D4", "F4"));
+		assertEquals(true, board.move("Promethee", "D4", "E4"));
+		assertEquals(true, board.move("Promethee", "D4", "D5"));
 	}
 	
 	@Test
 	public void bishopMove() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.setBoard(BoardBuilder.initializeTestPosition());
 		board.displayBoard();
 		
 		// Wrong moves
-		assertEquals(false, board.move("David", "F5", "G4"));
-		assertEquals(false, board.move("David", "F5", "H3"));
-		assertEquals(false, board.move("David", "F5", "D3"));
-		assertEquals(false, board.move("David", "F5", "C2"));
-		assertEquals(false, board.move("David", "F5", "C3"));
-		assertEquals(false, board.move("David", "F5", "H7"));
+		assertEquals(false, board.move("Promethee", "F5", "G4"));
+		assertEquals(false, board.move("Promethee", "F5", "H3"));
+		assertEquals(false, board.move("Promethee", "F5", "D3"));
+		assertEquals(false, board.move("Promethee", "F5", "C2"));
+		assertEquals(false, board.move("Promethee", "F5", "C3"));
+		assertEquals(false, board.move("Promethee", "F5", "H7"));
 
 		// Right moves
-		assertEquals(true, board.move("David", "F5", "C8"));
-		assertEquals(true, board.move("David", "F5", "D7"));
-		assertEquals(true, board.move("David", "F5", "E6"));
-		assertEquals(true, board.move("David", "F5", "G6"));
-		assertEquals(true, board.move("David", "F5", "E4"));
+		assertEquals(true, board.move("Promethee", "F5", "C8"));
+		assertEquals(true, board.move("Promethee", "F5", "D7"));
+		assertEquals(true, board.move("Promethee", "F5", "E6"));
+		assertEquals(true, board.move("Promethee", "F5", "G6"));
+		assertEquals(true, board.move("Promethee", "F5", "E4"));
 	}
 	
 	@Test
 	public void queenMove() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.setBoard(BoardBuilder.initializeTestPosition());
 		board.displayBoard();
 		
 		// Wrong moves
-		assertEquals(false, board.move("David", "E5", "F5"));
-		assertEquals(false, board.move("David", "E5", "G5"));
-		assertEquals(false, board.move("David", "E5", "H5"));
-		assertEquals(false, board.move("David", "E5", "H3"));
-		assertEquals(false, board.move("David", "E5", "C7"));
-		assertEquals(false, board.move("David", "E5", "B8"));
-		assertEquals(false, board.move("David", "E5", "E1"));
-		assertEquals(false, board.move("David", "E5", "E2"));
-		assertEquals(false, board.move("David", "E5", "E8"));
-		assertEquals(false, board.move("David", "E5", "D4"));
-		assertEquals(false, board.move("David", "E5", "C3"));
+		assertEquals(false, board.move("Promethee", "E5", "F5"));
+		assertEquals(false, board.move("Promethee", "E5", "G5"));
+		assertEquals(false, board.move("Promethee", "E5", "H5"));
+		assertEquals(false, board.move("Promethee", "E5", "H3"));
+		assertEquals(false, board.move("Promethee", "E5", "C7"));
+		assertEquals(false, board.move("Promethee", "E5", "B8"));
+		assertEquals(false, board.move("Promethee", "E5", "E1"));
+		assertEquals(false, board.move("Promethee", "E5", "E2"));
+		assertEquals(false, board.move("Promethee", "E5", "E8"));
+		assertEquals(false, board.move("Promethee", "E5", "D4"));
+		assertEquals(false, board.move("Promethee", "E5", "C3"));
 
 		// Right moves
-		assertEquals(true, board.move("David", "E5", "E3"));
-		assertEquals(true, board.move("David", "E5", "E4"));
-		assertEquals(true, board.move("David", "E5", "E6"));
-		assertEquals(true, board.move("David", "E5", "E7"));
-		assertEquals(true, board.move("David", "E5", "F6"));
-		assertEquals(true, board.move("David", "E5", "G7"));
-		assertEquals(true, board.move("David", "E5", "A5"));
-		assertEquals(true, board.move("David", "E5", "B5"));
-		assertEquals(true, board.move("David", "E5", "C5"));
-		assertEquals(true, board.move("David", "E5", "D5"));
-		assertEquals(true, board.move("David", "E5", "F4"));
+		assertEquals(true, board.move("Promethee", "E5", "E3"));
+		assertEquals(true, board.move("Promethee", "E5", "E4"));
+		assertEquals(true, board.move("Promethee", "E5", "E6"));
+		assertEquals(true, board.move("Promethee", "E5", "E7"));
+		assertEquals(true, board.move("Promethee", "E5", "F6"));
+		assertEquals(true, board.move("Promethee", "E5", "G7"));
+		assertEquals(true, board.move("Promethee", "E5", "A5"));
+		assertEquals(true, board.move("Promethee", "E5", "B5"));
+		assertEquals(true, board.move("Promethee", "E5", "C5"));
+		assertEquals(true, board.move("Promethee", "E5", "D5"));
+		assertEquals(true, board.move("Promethee", "E5", "F4"));
 
 	}
 	
 	@Test
 	public void knightMove() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.setBoard(BoardBuilder.initializeTestPosition());
 		board.displayBoard();
 		
 		// Wrong moves
-		assertEquals(false, board.move("David", "F3", "H2"));
-		assertEquals(false, board.move("David", "F3", "E1"));
-		assertEquals(false, board.move("David", "F3", "D4"));
-		assertEquals(false, board.move("David", "F3", "E5"));
-		assertEquals(false, board.move("David", "F3", "A4"));
-		assertEquals(false, board.move("David", "F3", "A3"));
-		assertEquals(false, board.move("David", "F3", "B8"));
+		assertEquals(false, board.move("Promethee", "F3", "H2"));
+		assertEquals(false, board.move("Promethee", "F3", "E1"));
+		assertEquals(false, board.move("Promethee", "F3", "D4"));
+		assertEquals(false, board.move("Promethee", "F3", "E5"));
+		assertEquals(false, board.move("Promethee", "F3", "A4"));
+		assertEquals(false, board.move("Promethee", "F3", "A3"));
+		assertEquals(false, board.move("Promethee", "F3", "B8"));
 
 
 		// Right moves
-		assertEquals(true, board.move("David", "F3", "H4"));
-		assertEquals(true, board.move("David", "F3", "G1"));
-		assertEquals(true, board.move("David", "F3", "D2"));
-		assertEquals(true, board.move("David", "F3", "G5"));
+		assertEquals(true, board.move("Promethee", "F3", "H4"));
+		assertEquals(true, board.move("Promethee", "F3", "G1"));
+		assertEquals(true, board.move("Promethee", "F3", "D2"));
+		assertEquals(true, board.move("Promethee", "F3", "G5"));
 	}
 	
 	@Test
 	public void pawnMove() throws Exception {
-		Board board = new Board("David", "Goliath");
+		Board board = new Board("Promethee", "Epimethee");
 		board.setBoard(BoardBuilder.initializeTestPosition());
 		board.displayBoard();
 		
 		// Wrong moves
-		assertEquals(false, board.move("David", "D3", "D4"));
-		assertEquals(false, board.move("David", "D3", "E4"));
-		assertEquals(false, board.move("David", "D3", "D4"));
-		assertEquals(true, board.move("David", "H2", "H4"));
-		assertEquals(true, board.move("David", "D3", "A4"));
-		assertEquals(true, board.move("David", "D3", "A3"));
+		// Pawn white can't go back
+		assertEquals(false, board.move("Promethee", "H2", "H1"));
+		assertEquals(false, board.move("Promethee", "G4", "G2"));
+		assertEquals(false, board.move("Promethee", "G4", "G3"));
+
+		
+		//Pawn black can't go back
+		assertEquals(false, board.move("Promethee", "C4", "C5"));
+		assertEquals(false, board.move("Promethee", "G4", "C6"));
+		
+		assertEquals(false, board.move("Promethee", "D3", "D4"));
+		assertEquals(false, board.move("Promethee", "D3", "E4"));
+		assertEquals(false, board.move("Promethee", "D3", "A4"));
+		assertEquals(false, board.move("Promethee", "D3", "A3"));
+		assertEquals(false, board.move("Promethee", "G4", "D2"));
+		assertEquals(false, board.move("Promethee", "H2", "H4"));
 
 
 		// Right moves
-		assertEquals(true, board.move("David", "D3", "C4"));
-		assertEquals(true, board.move("David", "E2", "E3"));
-		assertEquals(true, board.move("David", "E2", "E4"));
-		assertEquals(true, board.move("David", "H2", "H3"));
-		assertEquals(true, board.move("David", "D2", "C4"));
+		//Pawn black can't go back
+		assertEquals(true, board.move("Promethee", "C4", "C3"));
+		assertEquals(true, board.move("Promethee", "D3", "C4"));
+		assertEquals(true, board.move("Promethee", "E2", "E3"));
+		assertEquals(true, board.move("Promethee", "E2", "E4"));
+		assertEquals(true, board.move("Promethee", "H2", "H3"));
+		assertEquals(true, board.move("Promethee", "D3", "C4"));
+		assertEquals(true, board.move("Promethee", "B2", "B4"));
+
 
 	}
 }
